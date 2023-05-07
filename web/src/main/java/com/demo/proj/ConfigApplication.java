a2e1e278bf0b6ac3697220254f0e9b8731ed0fb8
jackeylove20XX <70844862+jackeylove20XX@users.noreply.github.com>
2023-05-07 21:42:58 +0800

    1

---
+++ web/src/main/java/com/demo/proj/ConfigApplication.java
@@ -0,0 +1,16 @@
+package com.demo.proj;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigApplication.class).run(args);
+    }
+
+}
+
+
